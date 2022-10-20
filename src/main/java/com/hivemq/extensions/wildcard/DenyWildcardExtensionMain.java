9b9015f480f0d8efe95045c274386d04c74be0f0
LukasHiveMQ <lukas.brand@hivemq.com>
2022-10-20 14:32:17 +0200

    Updated License headers

--- src/main/java/com/hivemq/extensions/wildcard/DenyWildcardExtensionMain.java
+++ src/main/java/com/hivemq/extensions/wildcard/DenyWildcardExtensionMain.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2018 dc-square GmbH
+ * Copyright 2019-present HiveMQ GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,10 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.hivemq.extensions.wildcard;
 
-import com.hivemq.extensions.wildcard.callbacks.DenyWildcardAuthorizer;
 import com.hivemq.extension.sdk.api.ExtensionMain;
 import com.hivemq.extension.sdk.api.annotations.NotNull;
 import com.hivemq.extension.sdk.api.parameter.ExtensionStartInput;
@@ -24,6 +22,7 @@ import com.hivemq.extension.sdk.api.parameter.ExtensionStartOutput;
 import com.hivemq.extension.sdk.api.parameter.ExtensionStopInput;
 import com.hivemq.extension.sdk.api.parameter.ExtensionStopOutput;
 import com.hivemq.extension.sdk.api.services.Services;
+import com.hivemq.extensions.wildcard.callbacks.DenyWildcardAuthorizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
