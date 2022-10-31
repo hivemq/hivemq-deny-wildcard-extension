00e6ee7850be72728ba75fff8411488b31fff381
Silvio Giebl <silvio.giebl@hivemq.com>
2022-11-01 00:26:27 +0100

    Rename package .wildcard -> .denywildcard

--- src/main/java/com/hivemq/extensions/denywildcard/DenyWildcardExtensionMain.java
+++ src/main/java/com/hivemq/extensions/denywildcard/DenyWildcardExtensionMain.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.hivemq.extensions.wildcard;
+package com.hivemq.extensions.denywildcard;
 
 import com.hivemq.extension.sdk.api.ExtensionMain;
 import com.hivemq.extension.sdk.api.annotations.NotNull;
